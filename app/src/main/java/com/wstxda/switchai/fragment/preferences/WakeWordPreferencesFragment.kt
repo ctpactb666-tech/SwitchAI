7d37c5a5c9047dac78239a2f21e4b6d3122a19b9
ctpactb666-tech <ctpactb666@gmail.com>
2026-09-23 23:53:12 +0300

    Add wake word settings screen

---
+++ app/src/main/java/com/wstxda/switchai/fragment/preferences/WakeWordPreferencesFragment.kt
@@ -0,0 +1,8 @@
+package com.wstxda.switchai.fragment.preferences
+
+import com.wstxda.switchai.R
+import com.wstxda.switchai.fragment.BasePreferenceFragment
+
+class WakeWordPreferencesFragment : BasePreferenceFragment() {
+    override val preferencesResId: Int get() = R.xml.preferences_wake_word
+}
