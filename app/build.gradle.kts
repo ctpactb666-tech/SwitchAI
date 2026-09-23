28834230792582ad0d8435320eb428df1d32dfc5
ctpactb666-tech <ctpactb666@gmail.com>
2026-09-24 00:17:04 +0300

    Add sherpa-onnx Android dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     implementation(libs.aboutlibraries.view)
     implementation(libs.markdown.core)
     implementation(libs.markdown.linkify)
+    implementation(libs.sherpa.onnx)
 }
