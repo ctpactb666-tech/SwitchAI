4000e2c6fba163eb942906626e1431a58b57bd85
ctpactb666-tech <ctpactb666@gmail.com>
2026-09-24 11:46:50 +0300

    Add SwitchAI home fragment

---
+++ app/src/main/java/com/wstxda/switchai/fragment/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.wstxda.switchai.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.wstxda.switchai.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<View>(R.id.openVoiceActivation).setOnClickListener {
+            findNavController().navigate(R.id.wakeWordPreferencesFragment)
+        }
+        val cards = listOf(
+            R.id.cardChatGpt,
+            R.id.cardGemini,
+            R.id.cardClaude,
+            R.id.cardAlice,
+            R.id.cardCopilot,
+            R.id.cardGrok,
+        )
+        cards.forEach { id ->
+            view.findViewById<View>(id).setOnClickListener {
+                findNavController().navigate(R.id.assistantsFragment)
+            }
+        }
+    }
+}
