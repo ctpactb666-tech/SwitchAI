13105e1770f109e7282d8566e00062241545f6aa
ctpactb666-tech <ctpactb666@gmail.com>
2026-09-24 22:55:42 +0300

    Wire new home controls

--- app/src/main/java/com/wstxda/switchai/fragment/HomeFragment.kt
+++ app/src/main/java/com/wstxda/switchai/fragment/HomeFragment.kt
@@ -9,8 +9,18 @@ import com.wstxda.switchai.R
 class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view.findViewById<View>(R.id.openVoiceActivation).setOnClickListener {
-            findNavController().navigate(R.id.wakeWordPreferencesFragment)
+            findNavController().navigate(R.id.voiceActivationFragment)
         }
+        view.findViewById<View>(R.id.startListening).setOnClickListener {
+            findNavController().navigate(R.id.listeningFragment)
+        }
+        view.findViewById<View>(R.id.openPhrasesQuick).setOnClickListener {
+            findNavController().navigate(R.id.phrasesFragment)
+        }
+        view.findViewById<View>(R.id.openAssistantsQuick).setOnClickListener {
+            findNavController().navigate(R.id.assistantsFragment)
+        }
+
         val cards = listOf(
             R.id.cardChatGpt,
             R.id.cardGemini,
