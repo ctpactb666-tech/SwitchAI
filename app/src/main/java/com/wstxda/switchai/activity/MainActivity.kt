023f6dd11d055a99248bf970605326bec79bbc95
ctpactb666-tech <ctpactb666@gmail.com>
2026-09-24 11:50:46 +0300

    Wire new bottom navigation to app destinations

--- app/src/main/java/com/wstxda/switchai/activity/MainActivity.kt
+++ app/src/main/java/com/wstxda/switchai/activity/MainActivity.kt
@@ -2,7 +2,12 @@ package com.wstxda.switchai.activity
 
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.wstxda.switchai.R
 import com.wstxda.switchai.databinding.ActivityMainBinding
 import com.wstxda.switchai.service.UpdaterService
 
@@ -15,6 +20,37 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navController = navHost.navController
+        setupBottomNavigation(binding.bottomNavigation, navController)
+
         UpdaterService.checkForUpdatesAuto(lifecycleScope, this, supportFragmentManager)
+    }
+
+    private fun setupBottomNavigation(
+        bottomNavigation: BottomNavigationView,
+        navController: NavController,
+    ) {
+        bottomNavigation.setOnItemSelectedListener { item ->
+            val current = navController.currentDestination?.id
+            if (current != item.itemId) {
+                navController.navigate(item.itemId)
+            }
+            true
+        }
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment,
+                R.id.assistantsFragment,
+                R.id.commandsFragment,
+                R.id.settingsFragment -> {
+                    bottomNavigation.visibility = android.view.View.VISIBLE
+                    bottomNavigation.menu.findItem(destination.id)?.isChecked = true
+                }
+                else -> bottomNavigation.visibility = android.view.View.VISIBLE
+            }
+        }
     }
 }
